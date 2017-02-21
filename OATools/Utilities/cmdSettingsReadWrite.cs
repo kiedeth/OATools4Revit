afc7f16d28c2b272ba8a3e77f73a615bc4064aab
kiedeth <kiedeth@icloud.com>
2017-02-21 17:48:50 -0500

    with working settings file and directory create

--- OATools/Utilities/cmdSettingsReadWrite.cs
+++ OATools/Utilities/cmdSettingsReadWrite.cs
@@ -10,36 +10,81 @@ namespace OATools.Utilities
 {
     class cmdSettingsReadWrite
     {
+        static string appData = Environment.ExpandEnvironmentVariables("%appdata%"); //this gives C:\Users\<userName>\AppData\Roaming
+        static string directory = appData + "/OATools";
+        static string fileName = "OATools_Settings";
+        static string fileType = "ini";
+        public static string path = directory + "/" + fileName + "." + fileType;
+
+        public void Check4SettingsDirectory()
+        {
+            //Check for directory, if no directory exists create it
+            if (!Directory.Exists(directory)) Directory.CreateDirectory(directory);
+        }
+        
+        public void CreateSettingsFile()
+        {
+            //var directory = Environment.ExpandEnvironmentVariables("%appdata%" + "/OATools");
+
+            Check4SettingsDirectory();
+
+            //Set the file path
+            var path = directory + "/OATools_Settings.ini";
+            //var path = @"C:/Users/jschaad/Documents/Visual Studio 2015/Projects/OATools/OATools/DNotes/OAToolsSettings.ini";
+
+            //Check for file
+            if (!File.Exists(path))
+            {
+                //if no file exists then create the file
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    String header = "<|OA TOOLS 4 REVIT SETTINGS FILE - DO NOT EDIT MANUALLY|>";
+                    sw.WriteLine(header);
+                    sw.Close();
+                }
+            }
+        }
+
 
         public void WriteSetting(String tag, String settingToWrite)
         {
-            var path = @"C:/Users/jschaad/Documents/Visual Studio 2015/Projects/OATools/OATools/DNotes/OAToolsSettings.ini";
-            //String header = "<<<THIS IS AN OA TOOLS 4 REVIT SETTINGS FILE - DO NOT EDIT MANUALLY>>>";
-            //string line;
+            //var directory = Environment.ExpandEnvironmentVariables("%appdata%" + "/OATools");
 
 
+            //Check for directory, if no directory exists create it
+            //if (!Directory.Exists(directory)) CreateSettingsFile();
 
-            File.WriteAllLines(path,
-            //WriteAllLines takes a string[] and writes it to a file
+            //Set the file path
+            //var path = directory + "/OATools_Settings.ini";
 
-            File.ReadAllLines(path)
-            //ReadAllLines reads a string[] from a file
-
-            .Select(x =>
-            // .Select lets us transform the data... So it's going to go over each string in the array (each line in the file), and do whatever we say to do.  It's going to execute the following code for each line in the file (and the line will be stored in "x" because i said "x =>" above)
-
+            //Check for file
+            if (!File.Exists(path))
             {
-                if (x.StartsWith(tag)) return tag + settingToWrite;
-                   // If the line starts with "ip = ", we want to change that line...
+                //if no file exists then create the file
+                CreateSettingsFile();
+            }
+            
 
-                  //else if (x.StartsWith("name = ")) return "name = " + box2.Text;
-                    // If the line starts with "name = ", we also want to change that line...
+            else
+            {
 
-                 return x;
-                 // If the line isn't one of the ones we want to change, we just return the original string (x) without making any changes
+                //WriteAllLines takes a string[] and writes it to a file
+                File.WriteAllLines(path,                
+
+                //ReadAllLines reads a string[] from a file
+                File.ReadAllLines(path)                
+
+                .Select(x =>
+
+                {
+                    if (x.StartsWith(tag)) return tag + settingToWrite;
+
+                //If line doesnt match anything just return
+                return x;
 
             }));
 
+            }
 
         }
 
@@ -47,79 +92,29 @@ namespace OATools.Utilities
 }
 
 
-//Stream l_fileStream = File.Open(path, FileMode.Open, FileAccess.ReadWrite);
-//StreamReader l_reader = new StreamReader(l_fileStream);
-//StreamWriter l_writer = new StreamWriter(l_fileStream);
 
 
-//using (StreamWriter sw = new StreamWriter(path))
-//{
-//    String prefix = "<|";
-//    String sufix = "|>";
-//    String header = "<<<THIS IS AN OA TOOLS 4 REVIT SETTINGS FILE - DO NOT EDIT MANUALLY>>>";
-//    sw.WriteLine(header);
+//            else
+//            {
 
 
-//    if (settingToWrite.StartsWith(tag))
-//    {
-//        sw.WriteLine(tag + settingToWrite);
-//    }
+//                File.WriteAllLines(path,
+//                //WriteAllLines takes a string[] and writes it to a file
 
-//}
+//                File.ReadAllLines(path)
+//                //ReadAllLines reads a string[] from a file
 
+//                .Select(x =>
+//                // .Select lets us transform the data... So it's going to go over each string in the array (each line in the file), and do whatever we say to do.  It's going to execute the following code for each line in the file (and the line will be stored in "x" because i said "x =>" above)
 
+//                {
+//    if (x.StartsWith(tag)) return tag + settingToWrite;
+//    // If the line starts with "ip = ", we want to change that line...
 
-//private static string findLine(string tag, string settingToWrite)
-//{
-//    // Determine whether a tag begins the string.
-//    if (settingToWrite.Trim().StartsWith(tag))
-//    {
-//        // Find the closing tag.
-//        int lastLocation = settingToWrite.IndexOf(tag);
-//        // Remove the tag.
-//        if (lastLocation >= 0)
-//        {
-//            settingToWrite = settingToWrite.Substring(lastLocation + 1);
+//    //else if (x.StartsWith("name = ")) return "name = " + box2.Text;
+//    // If the line starts with "name = ", we also want to change that line...
 
-//            // Remove any additional starting tags.
-//            settingToWrite = findLine(tag, settingToWrite);
-//        }
-//    }
+//    return x;
+//    // If the line isn't one of the ones we want to change, we just return the original string (x) without making any changes
 
-//    return settingToWrite;
-//}
-
-
-
-
-
-
-
-
-
-
-//public void Write()
-//{
-
-//    string[] names = new string[] { "[DNOTE SETTINGS], ", "TEST" };
-//    using (StreamWriter sw = new StreamWriter("C:/Users/jschaad/Documents/Visual Studio 2015/Projects/OATools/OATools/DNotes/OAToolsSettings.ini"))
-//    {
-
-//        foreach (string s in names)
-//        {
-//            sw.WriteLine(s);
-//        }
-//    }
-
-//    // Read and show each line from the file.
-//    //string line = "";
-//    //using (StreamReader sr = new StreamReader("C:/Users/jschaad/Documents/Visual Studio 2015/Projects/OATools/OATools/DNotes/OAToolsSettings.ini"))
-//    //{
-//    //    while ((line = sr.ReadLine()) != null)
-//    //    {
-//    //        Console.WriteLine(line);
-//    //    }
-//    //}
-
-//    //Console.ReadKey();
-//}
+//}));
